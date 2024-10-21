6ba07d7a46bebdb5fc2011dda34a2a454e103ba5
Christopher Hakkaart <chris.hakkaart@seqera.io>
2024-10-21 09:46:55 +0200

    Add site

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,7 @@ import starlight from '@astrojs/starlight';
 
 // https://astro.build/config
 export default defineConfig({
+	site: 'https://christopher-hakkaart.github.io',
 	integrations: [
 		starlight({
 			title: 'My Docs',
