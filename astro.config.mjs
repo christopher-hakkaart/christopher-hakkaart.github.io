4b0af41c7eefbe92a675b08101fd43c2d48b1679
Christopher Hakkaart <chris.hakkaart@seqera.io>
2024-10-22 13:51:37 +0200

    Added toy examples

--- astro.config.mjs
+++ astro.config.mjs
@@ -23,6 +23,10 @@ export default defineConfig({
 					label: 'Reference',
 					autogenerate: { directory: 'reference' },
 				},
+				{
+					label: 'nf-core',
+					autogenerate: { directory: 'nf-core' },
+				},
 			],
 		}),
 	],
